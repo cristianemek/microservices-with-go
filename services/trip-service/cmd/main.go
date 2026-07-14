2980c07ed98eaa26084cf45cc2b838c608401655
Cristianemek <cristianrodriguezlugo@gmail.com>
2026-07-14 20:13:46 +0200

    Implementado apagado seguro en api gateway y en trip service

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -24,7 +29,30 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error: %v", err)
+	tripServiceErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("Trip service listening on: %v", server.Addr)
+		tripServiceErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-tripServiceErrors:
+		log.Printf("Error starting trip service: %v", err)
+
+	case sig := <-shutdown:
+		log.Printf("Servir is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop server gracefully: %v", err)
+			server.Close()
+		}
 	}
+
 }
